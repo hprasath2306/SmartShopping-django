526347daf41cf120fa62d5d9c1adf0282de42482
unknown <hprasathoff23066@gmail.com>
2024-01-25 15:32:45 +0530

    rectified a small bug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
 
 
 # Default primary key field type
